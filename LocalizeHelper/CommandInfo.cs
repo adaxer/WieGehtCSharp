eea0b460409c00b17426cedcb156af95a7edc1c2
Andreas Daxer <info@andreas-daxer.de>
2025-04-14 16:37:22 +0200

    Praxis - LocalizeHelper

---
+++ LocalizeHelper/CommandInfo.cs
@@ -0,0 +1,62 @@
+﻿using LocalizeHelper;
+using System.Globalization;
+
+namespace LocalizeHelper;
+
+public class CommandInfo
+{
+    private static CommandInfo _none = new(Command.None, string.Empty, string.Empty, []);
+    private static CommandInfo _invalid = new(Command.Invalid, string.Empty, string.Empty, []);
+    private string _filePath=string.Empty;
+
+    private CommandInfo(Command command, string inputText, string outputPath, string[] languageCodes)
+    {
+        Command = command;
+        LanguageCodes = languageCodes;
+        InputText = inputText;
+        OutputPath = outputPath;
+    }
+
+    public Command Command { get; }
+    public string InputText { get; }
+    public string OutputPath { get; }
+    public string FilePath => _filePath;
+    public string[] LanguageCodes { get; }
+
+    public static CommandInfo None => _none;
+    public static CommandInfo Invalid => _invalid;
+
+    public static CommandInfo Create(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            return None;
+        }
+        try
+        {
+            Command command = Enum.Parse<Command>(args[0]);
+            string content = File.ReadAllText(args[1]);
+            var codes = args
+                .Skip(2)
+                .Where(code => CultureInfo.GetCultures(CultureTypes.AllCultures)
+                    .Any(c => c.TwoLetterISOLanguageName.Equals(code, StringComparison.OrdinalIgnoreCase)))
+                .Select(code => code.ToUpperInvariant()) // oder ToUpperInvariant(), wenn du uppercase willst
+                .Distinct();
+            string outputPath = (command == Command.ResxToCsv) ? args[1].Replace("resx", "csv") : args[1].Replace("csv", "resx");
+            return new CommandInfo(command, content, outputPath, codes.ToArray()) { _filePath = args[1] };
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine(ex.Message);
+            return CommandInfo.Invalid;
+        }
+    }
+}
+
+public enum Command
+{
+    None,
+    ResxToCsv,
+    CsvToResx,
+    Invalid
+}
---
+++ LocalizeHelper/Program.cs
@@ -0,0 +1,114 @@
+﻿using System.Xml.Linq;
+
+namespace LocalizeHelper;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var commandInfo = CommandInfo.Create(args);
+        Action<CommandInfo> action = commandInfo.Command switch
+        {
+            Command.ResxToCsv => MakeCsv,
+            Command.CsvToResx => MakeResx,
+            Command.None => ShowMenu,
+            _ => ShowUsage,
+        };
+        action(commandInfo);
+    }
+
+    private static void MakeCsv(CommandInfo info)
+    {
+        try
+        {
+            XElement xml = XElement.Load(new StringReader(info.InputText));
+            var data = from element in xml.Elements("data")
+                       where element.Attribute("name") is { } && element.Element("value") is { }
+                       select new KeyValuePair<string, string>(element.Attribute("name")!.Value.ToString(), element.Element("value")!.Value.ToString());
+
+            string langTitles = string.Join(",", info.LanguageCodes.Select(c => $"\"{c}\""));
+            string langEmpties = string.Join(",", info.LanguageCodes.Select(c => $"\"\""));
+            Func<string, string, string, string> createLine = (key, value, rest) => $"\"{key}\",\"{value}\",{rest}";
+
+            List<string> csv = [createLine("Key", "DE", langTitles)];
+            csv = csv.Concat(data.Select(kvp => createLine(kvp.Key, kvp.Value, langEmpties))).ToList();
+
+            File.WriteAllLines(info.OutputPath, csv);
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine($"MakeCsv nicht erfolgreich, Fehlermeldung: {ex.Message}");
+        }
+    }
+
+    private static void MakeResx(CommandInfo info)
+    {
+        try
+        {
+            var csv = info.InputText
+                .Trim('"')
+                .Replace("\",\"", "||")
+                .Replace("\"\n\"", "|z|")
+                .Split("|z|", StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => s.Split("||", StringSplitOptions.None));
+
+            var languages = csv
+                .First()
+                .Skip(2)
+                .ToList();
+
+            for (int i = 0; i < languages.Count; i++)
+            {
+                var fileName = info.OutputPath.Replace("translation", languages[i]);
+                var data = csv.Skip(1).ToDictionary(a => a[0], a => a[i + 2]);
+                CreateResx(fileName, data);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine($"MakeCsv nicht erfolgreich, Fehlermeldung: {ex.Message}");
+        }
+    }
+
+    private static void CreateResx(string fileName, Dictionary<string, string> data)
+    {
+        var content = data.Select(kvp =>
+            new XElement("data",
+                new XAttribute("name", kvp.Key),
+                new XAttribute(XNamespace.Xml + "space", "preserve"),
+                new XElement("value", kvp.Value)));
+        var xml = new XElement("root", _resxHeaders, content);
+        xml.Save(fileName);
+    }
+
+    private static readonly IReadOnlyList<XElement> _resxHeaders = new List<XElement>
+    {
+        new("resheader",
+            new XAttribute("name", "resmimetype"),
+            new XElement("value", "text/microsoft-resx")),
+        new("resheader",
+            new XAttribute("name", "version"),
+            new XElement("value", "2.0")),
+        new("resheader",
+            new XAttribute("name", "reader"),
+            new XElement("value", "System.Resources.ResXResourceReader, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")),
+        new("resheader",
+            new XAttribute("name", "writer"),
+            new XElement("value", "System.Resources.ResXResourceWriter, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"))
+    }.AsReadOnly();
+    private static void ShowUsage(CommandInfo info)
+    {
+        var usage = "\nVerwendung:\nLocalizeHelper [ResxToCsv|Csv2Resx] <filePath> <languages>\n";
+        usage += "\nBeispiel:\nLocalizeHelper ResxToCsv my.de.resx en fr\n";
+        usage += "Erzeugt aus my.de.resx die Datei my.de.csv\n\n";
+        usage += "LocalizeHelper CsvToResx my.translation.csv en fr\n";
+        usage += "Erzeugt aus my.translation.csv die Dateien my.EN.resx und my.FR.resx";
+        usage += "\n\nLocalizeHelper\nzeigt interaktives Menü.";
+        Console.WriteLine(usage);
+    }
+
+    private static void ShowMenu(CommandInfo info)
+    {
+        Console.WriteLine("Menu: ...");
+    }
+}
