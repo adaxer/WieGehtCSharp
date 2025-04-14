5e7cecde7d6b969d6c05c9c0a935706d6f12633c
Andreas Daxer <info@andreas-daxer.de>
2025-04-14 16:21:57 +0200

    Struktur - EVA

---
+++ ProjektStruktur/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ProjektStruktur;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Eingabe
+        Console.WriteLine("Hallo - wie ist der werte Name");
+        var name = Console.ReadLine();
+        Console.WriteLine("Und das Alter, wenn die Frage erlaubt ist");
+        var age = int.Parse(Console.ReadLine());
+
+        // Verarbeitung
+        var greeting = "Hallo " + name + ", herzlichen Glückwunsch zum " + age.ToString()+".";
+
+        // Ausgabe
+        Console.WriteLine(greeting);
+    }
+}
