5d1abc1d8623870222c057928413830e2bf4f79f
Andreas Daxer <info@andreas-daxer.de>
2025-04-14 16:28:26 +0200

    Struktur - args

--- ProjektStruktur/Program.cs
+++ ProjektStruktur/Program.cs
@@ -4,14 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
+        string name;
+        int age;
+
         // Eingabe
-        Console.WriteLine("Hallo - wie ist der werte Name");
-        var name = Console.ReadLine();
-        Console.WriteLine("Und das Alter, wenn die Frage erlaubt ist");
-        var age = int.Parse(Console.ReadLine());
+        if (args.Length == 2)
+        {
+            name = args[0];
+            age = int.Parse(args[1]);
+        }
+        else
+        {
+            Console.WriteLine("Hallo - wie ist der werte Name");
+            name = Console.ReadLine();
+            Console.WriteLine("Und das Alter, wenn die Frage erlaubt ist");
+            age = int.Parse(Console.ReadLine());
+        }
 
         // Verarbeitung
-        var greeting = "Hallo " + name + ", herzlichen Glückwunsch zum " + age.ToString()+".";
+        var greeting = "Hallo " + name + ", herzlichen Glückwunsch zum " + age.ToString() + ".";
 
         // Ausgabe
         Console.WriteLine(greeting);
