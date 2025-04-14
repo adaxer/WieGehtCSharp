991163c151c03d346d4144297e78ed6e293a66a9
Andreas Daxer <info@andreas-daxer.de>
2025-04-14 16:36:11 +0200

    ConsoleClass - FFmpeg

---
+++ HoldTheLine/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HoldTheLine;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Start");
+        Console.CursorVisible = false;
+        for(int zeile=0; zeile<100; zeile++)
+        {
+            await Task.Delay(200);
+            string output=$"\rZeile: {zeile.ToString().PadLeft(4, '0')}";
+            Console.Write(output);
+        }
+    }
+}
