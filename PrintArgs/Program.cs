c5da0e7b90bbc50da19ab9f2cd94439cb4e6e5af
Andreas Daxer <info@andreas-daxer.de>
2025-04-14 16:33:11 +0200

    Struktur - Übung Lösung

---
+++ PrintArgs/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PrintArgs;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ich habe folgende Argumente erhalten, alphabetisch sortiert:");
+        foreach (var arg in args.Order())
+        {
+            Console.WriteLine(arg);
+        }
+    }
+}
