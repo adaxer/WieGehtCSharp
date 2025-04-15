ece9568f866fe9ba0d7221d1f2747aac095db43f
Andreas Daxer <info@andreas-daxer.de>
2025-04-15 11:40:48 +0200

    Erweitert - Process.Start

---
+++ LocalizeHelper/LocalizableApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+var sourcePath = Path.GetFullPath("..\\..\\..\\..\\bin\\debug\\net9.0");
+var resx = Path.Combine(sourcePath, "Demo.de.resx");
+
+File.Copy(resx, "Demo.de.resx", true);
+
+var localizeHelper = Path.Combine(sourcePath, "LocalizeHelper.exe");
+
+var info = new ProcessStartInfo
+{
+    FileName = localizeHelper,
+    WorkingDirectory = Environment.CurrentDirectory,
+    Arguments = string.Join(' ', args)
+};
+
+var process = Process.Start(info);
+await process!.WaitForExitAsync();
